c0e2e0405524ab3e2db1e9f3ab5d4b5e76bb648e
Brian <brianbaero@gmail.com>
2024-01-23 18:43:47 -0800

    Part 1 Exercises 1.1-1.5

---
+++ Part1/courseinfo/src/App.jsx
@@ -0,0 +1,34 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  let total = 0
+  course.parts.forEach(value => {total += value.exercises})
+
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      <p>{course.parts[0].name} {course.parts[0].exercises}</p>
+      <p>{course.parts[1].name} {course.parts[1].exercises}</p>
+      <p>{course.parts[2].name} {course.parts[2].exercises}</p>
+      <p>Number of exercises {total}</p>
+    </div>
+  )
+}
+
+export default App
