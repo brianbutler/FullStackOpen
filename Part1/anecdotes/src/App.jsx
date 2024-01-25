f5638f61e77d730c34c9533588f6685a77009cda
Brian <brianbaero@gmail.com>
2024-01-24 16:52:25 -0800

    1.13: anecdotes step2

--- Part1/anecdotes/src/App.jsx
+++ Part1/anecdotes/src/App.jsx
@@ -11,10 +11,10 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
-  const [selected, setSelected] = useState(0)
 
   const lenAnecdotes = anecdotes.length
+
+  const [selected, setSelected] = useState({index:0, votes:new Array(lenAnecdotes).fill(0)})
 
   function randomInteger(min, max) {
     return Math.floor(Math.random() * Math.floor(max - min)) + Math.floor(min);
@@ -23,12 +23,20 @@ const App = () => {
   const nextSelected = () => () => {
     let next = randomInteger(0, lenAnecdotes)
     console.log("Next: ", next)
-    setSelected(next)
+    setSelected({index:next, votes:{...selected.votes}})
+  }
+
+  const voteSelected = () => () => {
+    let newState = {index:selected.index, votes:{...selected.votes}}
+    newState.votes[selected.index] += 1
+    setSelected(newState)
   }
 
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
+      <p>{anecdotes[selected.index]}</p>
+      <p>has {selected.votes[selected.index]} votes</p>
+      <button onClick={voteSelected()}>vote</button>
       <button onClick={nextSelected()}>next anecdote</button>
     </div>
   )
